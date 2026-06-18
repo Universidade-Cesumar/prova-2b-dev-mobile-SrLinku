bdca0b8a3d99b1b039379fdf4278212cf79b344f
Lincoln <lincoln.alves.de.oliveira@gmail.com>
2026-06-18 19:48:36 -0300

    feat: implementa função validarRetirada

--- src/utils/validacoes.js
+++ src/utils/validacoes.js
@@ -1,4 +1,10 @@
-function validarRetirada(estoqueAtual, quantidadeRetirada) {}
+function validarRetirada(estoqueAtual, quantidadeRetirada) {
+  if (estoqueAtual < 0 || quantidadeRetirada < 0) {
+    return false;
+  }
+
+  return quantidadeRetirada <= estoqueAtual;
+}
 
 module.exports = {
   validarRetirada,
